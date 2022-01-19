80c75eb2dccba3fae2ab90b03c674f3fa71c52a2
akilalakshmanan <Akila.L@iiitb.ac.in>
2022-01-19 21:15:12 +0530

    Python program to add 2 integers

---
+++ sample.py
@@ -0,0 +1,10 @@
+# Printing Hello world in python
+
+print("Hello World!")
+
+# Adding two numbers in python
+print("Program to add two integers")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+sum = a + b
+print("The total value is ", sum)
