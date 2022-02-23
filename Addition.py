7e51ce699e6498da5a572171fa0054157381d31d
Akila Lakshmanan <77330852+akilalakshmanan@users.noreply.github.com>
2022-02-23 09:00:48 +0530

    Adding two numbers in python

---
+++ Addition.py
@@ -0,0 +1,8 @@
+
+# Adding two numbers in python
+print("Program to add two integers")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+sum = a + b
+print("The total value is ", sum)
+
